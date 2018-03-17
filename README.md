71cf63623baf0ffde67d752195eb3903780ae3c6
lafronte <36543152+lafronte@users.noreply.github.com>
2018-03-17 11:28:13 -0400

    Added checklist link to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mygolfapp
+
+Checklist is at the following link:
+
+https://www.manifest.ly/runs/253287/edit
--- screens/GolfersScreen.js
+++ screens/GolfersScreen.js
@@ -35,9 +35,7 @@ class GolfersScreen extends Component {
             <View style={styles.container}>
                 <View style={[styles.boxcontainer, styles.box1]}>
                     <Text>GolfersScreen</Text>
-                    <List
-                    
-                    />
+
                 </View>
                 <View style={[styles.boxcontainer, styles.box2]}>
                 <Button 
